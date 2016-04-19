99982995567f1b20f14bd6fbaf264555e0794aaa
tomhauswald <t.hauswald@yahoo.de>
2016-04-19 14:03:41 +0200

    Grammar files now support comments escaped by '//'. Cleaned up the grammar file and build script.

--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+# Get rules file name without extension.
 grammarfile=$(basename $1)
 name="${grammarfile%.*}"
-rm "${name}" # Delete old executable.
-mkdir "${name}.build"
+
+# Delete old executable.
+rm "${name}" 
+
+# Create / clear build directory.
+if [! -d "${name}.build" ]; then
+	mkdir "${name}.build"
+else
+	rm -f "${name}.build/*"
+fi
+
+# Remove all lines starting with / (comments)
+# and save the result as name.temp.cf 
+# inside the build directory.
+grep -v '^ *//' $1 > "${name}.build/${name}.temp.cf"
+
+# Invoke BNFC.
 cd "${name}.build"
-rm -f * # Clear build directory.
-bnfc -m -cpp -l ../$1
+bnfc -m -cpp -l "${name}.temp.cf"
+
+# Invoke make.
 make
+
+# Move test executable to original directory.
 cd ..
 mv "${name}.build/Test${name}" "${name}"
