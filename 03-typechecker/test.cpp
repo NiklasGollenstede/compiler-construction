fb27c0ec7acb76155a6cbe622ce673f8b3f17502
tomhauswald <t.hauswald@yahoo.de>
2016-06-09 13:41:49 +0200

    Modified makefile to not clean when targetting 'run'. Updated test file.

--- 03-typechecker/test.cpp
+++ 03-typechecker/test.cpp
@@ -2,27 +2,44 @@ string to_string(int i) {
 	return "a number";
 }
 
-int main(int argc, string argv) {
-	int x = 1;
-	{
-		double x = 2.0;
-		{
-			string x = to_string(3);
-			{
-				bool x = true;
-				{
-					{
-						if(x)
-						{
-							x && true;
-						} else
-						{
-							bool y = false;
-							x = x || y;
-						}
-					}
-				}
-			}
-		}
-	}
+void test_operand_types() {
+
+	bool b0, b1;
+	b0 = true;
+	b1 = false;
+	b0 && b1;
+	b0 || b1;
+	
+	int i0, i1;
+	i0 = 0;
+	i1 = 1;
+	i0 + i1;
+	i0 - i1;
+	i0 * i1;
+	i0 / i1;
+	i0++;
+	i0--;
+	++i0;
+	--i0;
+	i0 > i1;
+	i0 >= i1;
+	i0 <= i1;
+	i0 < i1;
+		
+	double d0, d1;
+	d0 = 0.0;
+	d1 = 1.0;
+	d0 + d1;
+	d0 - d1;
+	d0 * d1;
+	d0 / d1;
+	d0 > d1;
+	d0 >= d1;
+	d0 <= d1;
+	d0 < d1;
+	
+	string s0, s1;
+	s0 = "hello";
+	s1 = "world";
+	s0 + s1 + to_string(i0);
 }
